e6f5a83b8ff925f526bd838c63f2cff7566ae95b
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-05-13 22:04:01 +0600

    api correction

--- routes/api/payment.js
+++ routes/api/payment.js
@@ -1,6 +1,6 @@
 const express = require("express");
+const router = express.Router();
 const paymentController = require("../../controllers/paymentController");
 
-const router = express.Router();
-
 router.post("/confirmpayment", paymentController);
+module.exports = router;

4172e908c5cbc7e14d27e425e0e8bdc099de2ef9
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-05-24 01:19:49 +0600

    stripe new test

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,33 +3,31 @@ const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY);
 // Assuming the amount is passed in the request body
 async function paymentController(req, res) {
   try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
+    const params = {
+      submit_type: "pay",
       mode: "payment",
-      line_items: req.body.item.map((item) => {
-        return {
-          price_data: {
-            currency: "usd",
-            course_data: {
-              classname: item.classname,
-              image: item.image,
-            },
-            unit_amount: item.price * 100,
-          },
-        };
-      }),
-      success_url: [
-        "https://creative-design-school.vercel.app/dashboard/enrolled-classes",
-        "http://localhost:5173/dashboard/enrolled-classes",
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      shipping_option: [
+        { shipping_rate: "shr_1PJglSAivobkXwZsE3kczdGA" },
+        { shipping_rate: "shr_1PJgncAivobkXwZsO5ZODteX" },
       ],
-      cancle_url: [
-        "https://creative-design-school.vercel.app/dashboard/selected-classes",
-        "http://localhost:5173/dashboard/selected-classes",
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: "{{PRICE_ID}}",
+          quantity: 1,
+        },
       ],
-    });
-    res.json({ url: session.url });
+      mode: "payment",
+      success_url: `${YOUR_DOMAIN}?success=true`,
+      cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    };
+    const session = await stripe.checkout.sessions.create(params);
+
+    res.redirect(303, session.url);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ statusCode: 500, message: error.message });
   }
 }
 

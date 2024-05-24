58cefd860e8da1c41d7ded34a0a50c8b3067c771
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-05-24 12:55:04 +0600

    another stripe test

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,32 +1,28 @@
 const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY);
+const YOUR_DOMAIN = "https://creative-school-design.onrender.com";
 
 // Assuming the amount is passed in the request body
 async function paymentController(req, res) {
+  const { classname, price, image } = req.body;
   try {
-    const params = {
-      submit_type: "pay",
-      mode: "payment",
+    const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
-      billing_address_collection: "auto",
-      // shipping_option: [
-      //   { shipping_rate: "shr_1PJglSAivobkXwZsE3kczdGA" },
-      //   { shipping_rate: "shr_1PJgncAivobkXwZsO5ZODteX" },
-      // ],
-      line_items: {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            classImage: req.body.item.image,
-            courseName: req.body.item.classname,
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: classname,
+              image: image,
+            },
+            unit_amount: price * 100,
           },
-          unit_amount: req.body.item.price * 100,
         },
-      },
-
-      success_url: `https://creative-design-school.vercel.app?success=true`,
-      cancel_url: `https://creative-design-school.vercel.app/?canceled=true`,
-    };
-    const session = await stripe.checkout.sessions.create(params);
+      ],
+      mode: "payment",
+      success_url: `${YOUR_DOMAIN}?success=true`,
+      cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    });
 
     res.redirect(303, session.url);
   } catch (error) {
